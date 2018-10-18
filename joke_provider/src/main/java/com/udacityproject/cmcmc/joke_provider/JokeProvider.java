705f23ef644afff1310617986117ff7c3136c3d7
Curtis <sharkydart@gmail.com>
2018-10-18 17:14:36 -0400

    first commit

---
+++ joke_provider/src/main/java/com/udacityproject/cmcmc/joke_provider/JokeProvider.java
@@ -0,0 +1,8 @@
+package com.udacityproject.cmcmc.joke_provider;
+
+public class JokeProvider {
+    public String makeJoke(){
+        //send a random joke, of the desired type, from the joke repository
+        return "This is a joke.";
+    }
+}
